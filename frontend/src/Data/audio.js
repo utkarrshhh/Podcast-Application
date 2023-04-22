7a21d3f7aaa18d42fb39ffd851447045d7b73ca2
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-04-22 13:48:40 +0530

    home page

--- frontend/src/Data/audio.js
+++ frontend/src/Data/audio.js
@@ -34,4 +34,39 @@ export const audio = [
     creatorVideo: "Indian Podcast Factory",
     timeVideo: "30 min",
   },
+  {
+    imageAudio:
+      "https://uploads-ssl.webflow.com/63a4333d6709521275441c77/6442da53d67d2638e4799a7d_Rectangle%20262.png",
+    categoryAudio: "CATEGORY NAME - AUDIO",
+    headingAudio:
+      "How are Indians living a happier life than any other countries?",
+    subHeadingAudio:
+      "Spirituality and mindfulness: Many Indians practice various forms of spirituality, such as yoga, meditation, and other mindfulness practices.",
+    creatorAudio: "Indian Podcast Factory",
+    timeAudio: "30 min",
+    logoAudio: (
+      <svg
+        width="67"
+        height="67"
+        viewBox="0 0 67 67"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle opacity="0.8" cx="33.5" cy="33.5" r="33.5" fill="#FF9800" />
+        <path
+          d="M45.6079 31.7679C46.9412 32.5377 46.9412 34.4622 45.6079 35.232L28.9461 44.8517C27.6128 45.6215 25.9461 44.6593 25.9461 43.1197L25.9461 23.8803C25.9461 22.3407 27.6128 21.3785 28.9461 22.1483L45.6079 31.7679Z"
+          fill="white"
+        />
+      </svg>
+    ),
+    imageVideo:
+      "https://uploads-ssl.webflow.com/63a4333d6709521275441c77/6442da53d67d2638e4799a7d_Rectangle%20262.png",
+    categoryVideo: "CATEGORY NAME - VIDEO",
+    headingVideo:
+      "How are Indians living a happier life than any other countries?",
+    subHeadingVideo:
+      "Spirituality and mindfulness: Many Indians practice various forms of spirituality, such as yoga, meditation, and other mindfulness practices.",
+    creatorVideo: "Indian Podcast Factory",
+    timeVideo: "30 min",
+  },
 ];
