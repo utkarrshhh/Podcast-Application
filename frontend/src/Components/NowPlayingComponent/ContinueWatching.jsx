4b5453be6dae538dc73cb6588923305f5cb9bd31
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-04-24 00:00:41 +0530

    continue watching

--- frontend/src/Components/NowPlayingComponent/ContinueWatching.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ContinueWatching() {
-  return (
-    <div className='continue'>
-        
-    </div>
-  )
-}
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBarHome from "../Components/NavBar/NavBarHome";
 import SideBar from "../Components/NavBar/SideBar";
 import "./Home.css";
-import ContinueWatching from "../Components/NowPlayingComponent/ContinueWatching";
+import ContinueWatching from "../Components/ContinueWatching/ContinueWatching";
 import NowPlaying from "../Components/NowPlayingComponent/NowPlaying";
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
           <div>
             <div className="nowPlayingHeader">Now Playing</div>
             <NowPlaying/>
-            <h2>Pick Up Where You Left Off</h2>
+            <div className="nowPlayingHeader">Pick Up Where You Left Off</div>
             <ContinueWatching/>
           </div>
         </div>
