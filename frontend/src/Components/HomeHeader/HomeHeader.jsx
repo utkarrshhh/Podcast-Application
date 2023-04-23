77b76cc3137fb8e035d2d416ca7d8462df84b1ed
Ajay Khatri <amajaying@gmail.com>
2023-04-23 20:19:20 +0530

    added Hovers

--- frontend/src/Components/HomeHeader/HomeHeader.jsx
+++ frontend/src/Components/HomeHeader/HomeHeader.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import "./HomeHeader.css"
+import React from "react";
+import "./HomeHeader.css";
+import { Link } from "react-router-dom";
 
 export default function HomeHeader() {
   return (
-    <div className='HomeHeader'>
-        <div className="headerText">
-            <div className="headingText no-flex">Listen your favorite <span className="yellow">audio</span> & <span className="blue">video</span> podcasts in one place!</div>
-            <div className="headerButton">
-                <div className="explorePodcastButton">
-                    <button>Explore Podcasts</button>
-                </div>
-                <div className="startListeningButton">
-                    <button>Start Listening</button>
-                </div>
-            </div>
+    <div className="HomeHeader">
+      <div className="headerText">
+        <div className="headingText no-flex">
+          Listen your favorite <span className="yellow">audio</span> &{" "}
+          <span className="blue">video</span> podcasts in one place!
         </div>
-        <div className="headerImage">
-            <img src="https://uploads-ssl.webflow.com/63a4333d6709521275441c77/6442b27e39465332e9c19c92_Group%20108.png" alt="" />
+        <div className="headerButton">
+          <div className="explorePodcastButton">
+            <Link to="/login">
+              <button>Explore Podcasts</button>
+            </Link>
+          </div>
+          <div className="startListeningButton">
+            <Link to="/signup">
+              <button>Start Listening</button>
+            </Link>
+          </div>
         </div>
+      </div>
+      <div className="headerImage">
+        <img
+          src="https://uploads-ssl.webflow.com/63a4333d6709521275441c77/6442b27e39465332e9c19c92_Group%20108.png"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
